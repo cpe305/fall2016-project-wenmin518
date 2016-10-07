f89aedabbe7d448ec23e8bd410af16ef8093cec5
Wen <wenmin.he518@gmail.com>
2016-10-07 11:04:45 -0700

    class implementation

---
+++ src/wenmin/ParkingStructure.java
@@ -0,0 +1,8 @@
+public class ParkingStructure{
+   private ParkingSpot parkingSpotArr;
+   private int numofavailableParkingSpot;
+   private int numofoccupiedParkingSpot;
+   private Point position;
+
+   public ParkingStruture(ParkingSpot parkingSpotArr, int numofavailableParkingSpot)
+}
