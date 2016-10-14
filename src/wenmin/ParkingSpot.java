670c8bbff3ce2d44eff621ee4233956bb47d3878
Wen <wenmin.he518@gmail.com>
2016-10-14 11:23:10 -0700

    upload

---
+++ src/wenmin/ParkingSpot.java
@@ -0,0 +1,5 @@
+package wenmin;
+
+public class ParkingSpot {
+
+}
--- src/wenmin/ParkingStructure.java
+++ src/wenmin/ParkingStructure.java
@@ -1,8 +1,30 @@
-public class ParkingStructure{
-   private ParkingSpot parkingSpotArr;
-   private int numofavailableParkingSpot;
-   private int numofoccupiedParkingSpot;
-   private Point position;
+package wenmin;
+import java.util.*;
 
-   public ParkingStruture(ParkingSpot parkingSpotArr, int numofavailableParkingSpot)
+public class ParkingStructure{
+   private ParkingSpot[] parkingSpotArr;
+   private int numofavailableParkingSpots;
+   private int numofoccupiedParkingSpots;
+   private GeoLoc position;
+
+   public ParkingStruture(ParkingSpot parkingSpotArr, 
+	int numofavailableParkingSpots, int numofoccupiedParkingSpots,
+	Point position) {
+	   this.parkingSpotArr = parkingSpotArr;
+	   this.nuumofavailableParkingSpots = numofavailableParkingSpots;
+	   this.numofoccupiedParkingSpots = numofoccupirdParkingSpots;
+	   this.position = position;
+   }
+   
+   public int getNumavailable() {
+	   reutrn numofavailableParkingSpots;
+   }
+   
+   public int getNumOccupied() {
+	  return numofoccupiedParkingSpots; 
+   }
+   
+   public Point getParkingStructurePosition() {
+	   return position;
+   }
 }
