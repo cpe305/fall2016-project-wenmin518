64936c549bcad8753bce126c8dbe6fd1cd26d3b1
Wen <wenmin.he518@gmail.com>
2016-10-20 18:35:45 -0700

    add classes

--- src/wenmin/FindClosestSpot.java
+++ src/wenmin/FindClosestSpot.java
@@ -2,17 +2,18 @@ package wenmin;
 import java.util.*;
 
 public class FindClosestSpot implements Observer {
-	private Geoloc userloc;
-	private ArrayList<ParkingStructure> strArr;
-	
-	public FindClosestSpot(Geoloc userloc, ArrayList<ParkingStructure> strArr) {
-		this.userloc = userloc;
-		this.strArr = strArr;
-	}
+//	private Geoloc userloc;
+//	private String fileName;
+//	private ArrayList<ParkingStructure> strArr;
+//	
+//	public FindClosestSpot(Geoloc userloc, ArrayList<ParkingStructure> strArr) {
+//		this.userloc = userloc;
+//		this.strArr = strArr;
+//	}
 	@Override
 	public void updateUserloc(Geoloc loc) {
-		userloc = loc;
 	}
+	
 	
 
 }
