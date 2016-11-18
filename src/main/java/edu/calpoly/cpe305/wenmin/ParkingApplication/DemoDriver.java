b8825d953e8923d7e5485e6ff13bc3f562d401e9
Wen <wenmin.he518@gmail.com>
2016-11-17 18:46:08 -0800

    add more classes

--- src/main/java/edu/calpoly/cpe305/wenmin/ParkingApplication/DemoDriver.java
+++ src/main/java/edu/calpoly/cpe305/wenmin/ParkingApplication/DemoDriver.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 
 import org.json.simple.parser.ParseException;
 
-public class JasonReadDriver {
+public class DemoDriver {
   private static ParkingPath parkingPath;
   private static ParkingStrJson structJson;
   private static Scanner scan;
@@ -24,29 +24,34 @@ public class JasonReadDriver {
       System.out.println("as x y");
       int xval = scan.nextInt();
       int yval = scan.nextInt();
-      if (xval > parkingPath.getRows() || yval > parkingPath.getCols()) {
-        System.out.println("your x or y is out of range");
-      } else {
-        Geoloc userLoc = new Geoloc(xval, yval);
-        System.out.println("You entered (" + xval + ", " + yval + ")");
-        System.out.println("Please Enter the type of parking spot that you are looking for:");
-        System.out.println("1: Compact. 2: Electric. 3: Handicap. 4: Normal");
-        int typeInput = scan.nextInt();
-        if (1 > typeInput && typeInput > 4) {
-          System.out.println("Your input is out of range");
-        } else {
-          User user = new User(userLoc, typeInput);
-          Calculation cal = new Calculation(parkingPath.getAdj(), parkingPath.getVisited(), user,
-              parkingPath.getRows(), parkingPath.getCols(), parkingPath.getNumVer(),
-              structJson.getStructArr());
-          System.out.println();
-          cal.printInfo(userLoc, structJson.getStructArr());
-        }
+      while (xval > parkingPath.getRows() || yval > parkingPath.getCols()) {
+        System.out.println("your x or y is out of range.");
+        System.out.println("Please Enter valid positions");
+        xval = scan.nextInt();
+        yval = scan.nextInt();
       }
-      System.out.println("Press Y if you want to continue");
-      permission = scan.nextLine();
+      Geoloc userLoc = new Geoloc(xval, yval);
+      System.out.println("You entered (" + xval + ", " + yval + ")");
+      System.out.println("Please Enter the type of parking spot that you are looking for:");
+      System.out.println("1: Compact. 2: Electric. 3: Handicap. 4: Normal");
+      int typeInput = scan.nextInt();
+      while (1 > typeInput || typeInput > 4) {
+        System.out.println("Your input is out of range");
+        System.out.println("Please enter valid type input");
+        typeInput = scan.nextInt();
+      }
+      User user = new User(userLoc, typeInput);
+      Calculation cal = new Calculation(parkingPath.getAdj(), parkingPath.getVisited(), user,
+          parkingPath.getRows(), parkingPath.getCols(), parkingPath.getNumVer(),
+          structJson.getStructArr());
+      System.out.println();
+      cal.printInfo(userLoc, structJson.getStructArr());
+
     }
+    System.out.println("Press Y if you want to continue");
+    permission = scan.nextLine();
   }
+
 
   /**
    * The main driver class.
---
+++ src/main/java/edu/calpoly/cpe305/wenmin/ParkingApplication/FirstDriver.java
@@ -0,0 +1,8 @@
+package main.java.edu.calpoly.cpe305.wenmin.ParkingApplication;
+
+public class FirstDriver {
+  public static void main(String[] args) {
+    System.out.println("somthing");
+  }
+
+}
