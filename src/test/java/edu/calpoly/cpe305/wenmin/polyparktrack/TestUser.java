0459a6297eeb297f38e41e7d34371d4c66154541
Wen <wenmin.he518@gmail.com>
2016-11-30 20:14:11 -0800

    Add more Junit Tests for TestUser and ParkingStrJson

--- src/test/java/edu/calpoly/cpe305/wenmin/polyparktrack/TestUser.java
+++ src/test/java/edu/calpoly/cpe305/wenmin/polyparktrack/TestUser.java
@@ -1,17 +1,27 @@
 package test.java.edu.calpoly.cpe305.wenmin.polyparktrack;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
 
 import org.junit.Test;
 
+import main.java.edu.calpoly.cpe305.wenmin.polyparktrack.Calculation;
 import main.java.edu.calpoly.cpe305.wenmin.polyparktrack.Geoloc;
+import main.java.edu.calpoly.cpe305.wenmin.polyparktrack.ParkingStructure;
 import main.java.edu.calpoly.cpe305.wenmin.polyparktrack.User;
 
 
 
 public class TestUser {
 
+  public int vert = 4;
+  public int[][] adj = new int[vert][vert];
+  public boolean[] visited = new boolean[vert];
+  public ArrayList<ParkingStructure> parkLoc = new ArrayList<ParkingStructure>();
+  
   @Test
   public void userTest1() {
     User user = new User(new Geoloc(10, 10), 3);
@@ -28,7 +38,21 @@ public class TestUser {
     assertEquals(user.getCarType(), 3);
     assertEquals(user.getPosition().getX(), 0);
     assertEquals(user.getPosition().getY(), 1);
-
+  }
+  
+  @Test
+  public void testUserEquals() {
+    
+    User user = new User(new Geoloc(0, 1), 3);
+    Calculation cal = new Calculation(adj, visited, user, vert, vert, vert * 2, parkLoc);
+    user.notifyObsType(1);
+    user.notifyObsLoc(new Geoloc(1, 1));
+    user.registerObs(cal);
+    user.unregisterObs();
+    assertFalse(user.equals(new User(new Geoloc(1, 1), 2)));
+    assertFalse(user.equals(null));
+    assertFalse(user.equals(new Geoloc(1, 1)));
+    
   }
 
 }
