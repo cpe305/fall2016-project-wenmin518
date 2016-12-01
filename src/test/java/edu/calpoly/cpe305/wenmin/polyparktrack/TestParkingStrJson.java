786c2279f76dd367515540bb973cff480bf19ea8
Wen <wenmin.he518@gmail.com>
2016-11-30 21:29:18 -0800

    Add JUnit tests for TestPakingStrJson

---
+++ src/test/java/edu/calpoly/cpe305/wenmin/polyparktrack/TestParkingStrJson.java
@@ -0,0 +1,26 @@
+package test.java.edu.calpoly.cpe305.wenmin.polyparktrack;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+
+import org.json.simple.parser.ParseException;
+import org.junit.Test;
+
+import main.java.edu.calpoly.cpe305.wenmin.polyparktrack.ParkingStrJson;
+
+public class TestParkingStrJson {
+  ParkingStrJson structJson = new ParkingStrJson();
+
+
+  @Test
+  public void testAllMethods() throws ParseException, IOException {
+    structJson.setParkLocFromFile(
+        "/Users/wenmin518/git/fall2016-project-wenmin518/jsonfiles/JsonTest2.json");
+    assertEquals(structJson.getStrcutre(0).getNumavailable(), 51);
+    assertEquals(structJson.getStructArr().get(1).getNumavailable(), 46);
+    structJson.setStrArrAt(0, structJson.getStrcutre(1));
+    assertEquals(structJson.getStructArr().get(0).getNumavailable(), 46);
+  }
+
+}
